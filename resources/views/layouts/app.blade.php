df2ef2897bba667f68bdfb867236b4eba00325d6
williamsduarte <williamduarteoficial@gmail.com>
2018-09-15 15:38:51 -0400

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<html lang="{{ app()->getLocale() }}">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+	<link href="img/favicon.png" rel="icon" type="image/png">
+	<link href="img/favicon.ico" rel="shortcut icon">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	<link rel="stylesheet" href="css/lib/lobipanel/lobipanel.min.css">
+	<link rel="stylesheet" href="css/separate/vendor/lobipanel.min.css">
+	<link rel="stylesheet" href="css/lib/jqueryui/jquery-ui.min.css">
+	<link rel="stylesheet" href="css/separate/pages/widgets.min.css">
+    <link rel="stylesheet" href="css/lib/font-awesome/font-awesome.min.css">
+    <link rel="stylesheet" href="css/lib/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body class="with-side-menu control-panel control-panel-compact">
+
+	@include('layouts.default.header')
+
+	<div class="mobile-menu-left-overlay"></div>
+
+	@include('layouts.default.sidebar')
+
+	@yield('content')
+
+	@include('layouts.default.tasks')
+
+	@include('layouts.default.scriptsbody')
+
+	<!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+
+</body>
+</html>
---
+++ webpack.mix.js
@@ -0,0 +1,23 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+// mix.js('resources/theme', 'public/js')
+//    .sass('resources/sass/app.scss', 'public/css');
+
+
+
+//mix.copy('resources/themes/startui/build/css/custom.css', 'public/css/custom.css');
+mix.copyDirectory('resources/themes/startui/build/css', 'public/css');
+mix.copyDirectory('resources/themes/startui/build/img', 'public/img');
+mix.copyDirectory('resources/themes/startui/build/js', 'public/js');
+mix.copyDirectory('resources/themes/startui/build/fonts', 'public/fonts');
