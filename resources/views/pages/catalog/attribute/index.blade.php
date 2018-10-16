21fb406339f53a644c6e19d8b9b16523813f7026
williamsduarte <williamduarteoficial@gmail.com>
2018-10-16 18:19:43 -0400

    new feature atributos refactory

--- resources/views/pages/catalog/attribute/index.blade.php
+++ resources/views/pages/catalog/attribute/index.blade.php
@@ -40,14 +40,42 @@
                         <thead>
                             <tr>
                                 <th>Atributo - Coluna para importação</th>
-                                <th width="300">Produtos vinculados</th>
+                                <th width="200">Produtos vinculados</th>
                                 <th class="tabledit-toolbar-column">Editar</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr id="1">
+                            <tr>
                                 <td class="tabledit-view-mode">
-                                    Last quarter revene
+                                    Gênero - grade-genero
+                                    <br>
+                                    <small>
+                                        Unissex,
+                                        Masculino,
+                                        Feminino
+                                    </small>
+                                </td>
+                                <td>
+                                    <span class="label label-default">1 produto vinculado</span>
+                                </td>
+                                <td style="white-space: nowrap; width: 1%;">
+                                    <div class="tabledit-toolbar btn-toolbar" style="text-align: left;">
+                                        <div class="btn-group btn-group-sm" style="float: none;"><button type="button" class="tabledit-edit-button btn btn-sm btn-default" style="float: none;"><span class="glyphicon glyphicon-pencil"></span></button><button type="button" class="tabledit-delete-button btn btn-sm btn-default" style="float: none;"><span class="glyphicon glyphicon-trash"></span></button></div>
+                                        <button type="button" class="tabledit-save-button btn btn-sm btn-success" style="display: none; float: none;">Save</button>
+                                        <button type="button" class="tabledit-confirm-button btn btn-sm btn-danger" style="display: none; float: none;">Confirm</button>
+                                        <button type="button" class="tabledit-restore-button btn btn-sm btn-warning" style="display: none; float: none;">Restore</button>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="tabledit-view-mode">
+                                    Gênero - grade-genero
+                                    <br>
+                                    <small>
+                                        Unissex,
+                                        Masculino,
+                                        Feminino
+                                    </small>
                                 </td>
                                 <td>
                                     <span class="label label-default">1 produto vinculado</span>
